1e9cf799fd69025ae79eb42efe9ab38b3a23c16b
Kostiantyn Kichehlov <kostiantyn.kichehlov@spryker.com>
2021-08-05 15:20:00 +0300

    CC-14090 Add constants

---
+++ src/SprykerEco/Shared/Unzer/UnzerConstants.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace SprykerEco\Shared\Unzer;
+
+interface UnzerConstants
+{
+    public const UNZER_PRIVATE_KEY = 'UNZER:UNZER_PRIVATE_KEY';
+    public const UNZER_PUBLIC_KEY = 'UNZER:UNZER_PUBLIC_KEY';
+    public const UNZER_MERCHANT_OPERATOR_PARTICIPANT_ID = 'UNZER:UNZER_MERCHANT_OPERATOR_PARTICIPANT_ID';
+}
