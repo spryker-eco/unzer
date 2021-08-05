d9d35229f2c2eceba7f276e5950648212a0dc0ba
Kostiantyn Kichehlov <kostiantyn.kichehlov@spryker.com>
2021-08-05 15:12:06 +0300

    CC-14090 Add base config and test files

---
+++ config/config.dist.php
@@ -0,0 +1,7 @@
+<?php
+
+use SprykerEco\Shared\Unzer\UnzerConstants;
+
+$config[UnzerConstants::UNZER_PRIVATE_KEY] = '';
+$config[UnzerConstants::UNZER_PUBLIC_KEY] = '';
+$config[UnzerConstants::UNZER_MERCHANT_OPERATOR_PARTICIPANT_ID] = '';
