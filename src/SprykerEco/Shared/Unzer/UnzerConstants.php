048f4326e0894575b7645b4a6f29fc5585992ef4
Kostiantyn Kichehlov <kostiantyn.kichehlov@spryker.com>
2021-09-20 16:57:25 +0300

    CC-14636 Add base files and structure

---
+++ src/SprykerEco/Shared/Unzer/UnzerConstants.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SprykerEco\Shared\Unzer;
+
+/**
+ * Declares global environment configuration keys. Do not use it for other class constants.
+ */
+interface UnzerConstants
+{
+    public const UNZER_AUTHORIZE_RETURN_URL = 'UNZER:UNZER_AUTHORIZE_RETURN_URL';
+    public const UNZER_CHARGE_RETURN_URL = 'UNZER:UNZER_CHARGE_RETURN_URL';
+    public const WEBHOOK_RETRIEVE_URL = 'UNZER:WEBHOOK_RETRIEVE_URL';
+}
