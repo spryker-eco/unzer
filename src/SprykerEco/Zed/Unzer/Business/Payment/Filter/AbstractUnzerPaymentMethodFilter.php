d571ad7aed06b3115efb4e7f78651048a7286f43
Michael Knappe <michael.knappe@spryker.com>
2021-12-14 18:12:49 +0100

    Introduced AbstractUnzerPaymentMethodFilter

---
+++ src/SprykerEco/Zed/Unzer/Business/Payment/Filter/AbstractUnzerPaymentMethodFilter.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * MIT License
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace SprykerEco\Zed\Unzer\Business\Payment\Filter;
+
+use Generated\Shared\Transfer\PaymentMethodTransfer;
+use Generated\Shared\Transfer\QuoteTransfer;
+use SprykerEco\Shared\Unzer\UnzerConfig as SharedUnzerConfig;
+use SprykerEco\Zed\Unzer\UnzerConfig;
+
+abstract class AbstractUnzerPaymentMethodFilter
+{
+    /**
+     * @var string
+     */
+    protected const MAIN_SELLER_KEY = 'main';
+
+    /**
+     * @var \SprykerEco\Zed\Unzer\UnzerConfig
+     */
+    protected $unzerConfig;
+
+    /**
+     * @param \SprykerEco\Zed\Unzer\UnzerConfig $unzerConfig
+     */
+    public function __construct(UnzerConfig $unzerConfig)
+    {
+        $this->unzerConfig = $unzerConfig;
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
+     *
+     * @return bool
+     */
+    protected function hasMultipleMerchants(QuoteTransfer $quoteTransfer): bool
+    {
+        $merchantReferences = [];
+
+        foreach ($quoteTransfer->getItems() as $itemTransfer) {
+            $merchantReference = $itemTransfer->getMerchantReference();
+
+            if ($merchantReference === null) {
+                $merchantReferences[] = static::MAIN_SELLER_KEY;
+
+                continue;
+            }
+
+            if (!in_array($merchantReference, $merchantReferences, true)) {
+                $merchantReferences[] = $merchantReference;
+            }
+        }
+
+        return count($merchantReferences) > 1;
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\PaymentMethodTransfer $paymentMethodTransfer
+     *
+     * @return bool
+     */
+    protected function isUnzerPaymentProvider(PaymentMethodTransfer $paymentMethodTransfer): bool
+    {
+        return strpos($paymentMethodTransfer->getPaymentMethodKey(), $this->unzerConfig->getPaymentProviderType()) !== false;
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\PaymentMethodTransfer $paymentMethodTransfer
+     *
+     * @return bool
+     */
+    protected function isMarketplace(PaymentMethodTransfer $paymentMethodTransfer): bool
+    {
+        return strpos($paymentMethodTransfer->getPaymentMethodKey(), SharedUnzerConfig::PLATFORM_MARKETPLACE) !== false;
+    }
+}
