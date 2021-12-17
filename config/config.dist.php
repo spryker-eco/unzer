0eeff0927e149a78849c1ca6da75b3d6c29c30a5
Kostiantyn Kichehlov <kostiantyn.kichehlov@spryker.com>
2021-12-17 14:30:27 +0200

    CC-14650 Fix CR notices

--- config/config.dist.php
+++ config/config.dist.php
@@ -8,4 +8,4 @@ $config[UnzerConstants::WEBHOOK_RETRIEVE_URL] = 'https://spryker.com';
 
 $config[UnzerConstants::MAIN_MARKETPLACE_KEYPAIR_ID] = '';
 $config[UnzerConstants::MAIN_REGULAR_KEYPAIR_ID] = '';
-$config[UnzerConstants::VAULT_DATA_TYPE] = 'UNZER_PRIVATE_KEY';
+$config[UnzerConstants::VAULT_DATA_TYPE] = 'UNZER_CREDENTIALS_PRIVATE_KEY';
