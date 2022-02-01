5358508a62de3b583eff8b97162bfe2f2c28de36
Kostiantyn Kichehlov <kostiantyn.kichehlov@spryker.com>
2022-02-01 11:28:30 +0200

    CC-14716 Added Unzer credentials deleter

--- src/SprykerEco/Zed/Unzer/Business/Credentials/UnzerCredentialsDeleter.php
+++ src/SprykerEco/Zed/Unzer/Business/Credentials/UnzerCredentialsDeleter.php
@@ -8,6 +8,7 @@ use Generated\Shared\Transfer\UnzerCredentialsCriteriaTransfer;
 use Generated\Shared\Transfer\UnzerCredentialsResponseTransfer;
 use Generated\Shared\Transfer\UnzerCredentialsTransfer;
 use SprykerEco\Shared\Unzer\UnzerConstants;
+use SprykerEco\Zed\Unzer\Business\Exception\UnzerException;
 use SprykerEco\Zed\Unzer\Persistence\UnzerEntityManagerInterface;
 use SprykerEco\Zed\Unzer\Persistence\UnzerRepositoryInterface;
 
@@ -46,7 +47,6 @@ class UnzerCredentialsDeleter implements UnzerCredentialsDeleterInterface
         $this->unzerEntityManager = $unzerEntityManager;
     }
 
-
     /**
      * @param UnzerCredentialsTransfer $unzerCredentialsTransfer
      *
@@ -62,13 +62,18 @@ class UnzerCredentialsDeleter implements UnzerCredentialsDeleterInterface
             return $unzerCredentialsResponseTransfer->setIsSuccessful(false);
         }
 
+        if ($unzerCredentialsTransfer->getType() === UnzerConstants::UNZER_CONFIG_TYPE_MAIN_MARKETPLACE) {
+            $isSuccessful = $this->deleteMarketplaceMainMerchantUnzerCredentials($unzerCredentialsTransfer);
+            if (!$isSuccessful) {
+                return $this->buildFailedUnzerCredentialsResponseTransfer($unzerCredentialsResponseTransfer);
+            }
+        }
+
         if ($this->unzerEntityManager->deleteUnzerCredentials($unzerCredentialsTransfer)) {
             return $unzerCredentialsResponseTransfer->setIsSuccessful(true);
         }
 
-        $unzerCredentialsResponseTransfer->addMessage((new MessageTransfer())->setMessage(static::MESSAGE_DELETE_FAILED));
-
-        return $unzerCredentialsResponseTransfer->setIsSuccessful(false);
+        return $this->buildFailedUnzerCredentialsResponseTransfer($unzerCredentialsResponseTransfer);
     }
 
     /**
@@ -91,6 +96,43 @@ class UnzerCredentialsDeleter implements UnzerCredentialsDeleterInterface
         );
         $childUnzerCredentialsCollectionTransfer = $this->unzerRepository->findUnzerCredentialsCollectionByCriteria($unzerCredentialsCriteriaTransfer);
 
-        return $childUnzerCredentialsCollectionTransfer->getUnzerCredentials()->count() !== 0;
+        return $childUnzerCredentialsCollectionTransfer->getUnzerCredentials()->count() > 1;
+    }
+
+    /**
+     * @param UnzerCredentialsTransfer $unzerCredentialsTransfer
+     *
+     * @return bool
+     */
+    protected function deleteMarketplaceMainMerchantUnzerCredentials(UnzerCredentialsTransfer $unzerCredentialsTransfer): bool
+    {
+        $unzerCredentialsCriteriaTransfer = (new UnzerCredentialsCriteriaTransfer())->setUnzerCredentialsConditions(
+            (new UnzerCredentialsConditionsTransfer())
+                ->addParentId($unzerCredentialsTransfer->getIdUnzerCredentials())
+                ->addType(UnzerConstants::UNZER_CONFIG_TYPE_MARKETPLACE_MAIN_MERCHANT)
+        );
+
+        $unzerCredentialsCollectionTransfer = $this->unzerRepository
+            ->findUnzerCredentialsCollectionByCriteria($unzerCredentialsCriteriaTransfer);
+
+        if ($unzerCredentialsCollectionTransfer->getUnzerCredentials()->count() !== 1) {
+            throw new UnzerException('Unzer Marketplace credentials should have 1 main merchant credentials!');
+        }
+
+        $mainMerchantUnzerCredentialsTransfer = $unzerCredentialsCollectionTransfer->getUnzerCredentials()[0];
+
+        return $this->unzerEntityManager->deleteUnzerCredentials($mainMerchantUnzerCredentialsTransfer);
+    }
+
+    /**
+     * @param UnzerCredentialsResponseTransfer $unzerCredentialsResponseTransfer
+     *
+     * @return UnzerCredentialsResponseTransfer
+     */
+    protected function buildFailedUnzerCredentialsResponseTransfer(UnzerCredentialsResponseTransfer $unzerCredentialsResponseTransfer): UnzerCredentialsResponseTransfer
+    {
+        $unzerCredentialsResponseTransfer->addMessage((new MessageTransfer())->setMessage(static::MESSAGE_DELETE_FAILED));
+
+        return $unzerCredentialsResponseTransfer->setIsSuccessful(false);
     }
 }
