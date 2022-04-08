33b80e914ba684fc5c2d708869c6375f854031a7
Michael Knappe <michael.knappe@spryker.com>
2022-04-08 16:47:25 +0200

    Removed blank line

--- config/config.dist.php
+++ config/config.dist.php
@@ -5,7 +5,6 @@ use SprykerEco\Shared\Unzer\UnzerConstants;
 $config[UnzerConstants::UNZER_AUTHORIZE_RETURN_URL] = 'https://spryker.com/checkout/success';
 $config[UnzerConstants::UNZER_CHARGE_RETURN_URL] = 'https://spryker.com/checkout/success';
 $config[UnzerConstants::WEBHOOK_RETRIEVE_URL] = 'https://spryker.com';
-
 $config[UnzerConstants::MAIN_MARKETPLACE_KEYPAIR_ID] = '';
 $config[UnzerConstants::MAIN_REGULAR_KEYPAIR_ID] = '';
 $config[UnzerConstants::VAULT_DATA_TYPE] = 'UNZER_CREDENTIALS_PRIVATE_KEY';
