6011e7878aec88988abcf5ff6e1d66497c1c091f
Kostiantyn Kichehlov <kostiantyn.kichehlov@spryker.com>
2022-05-31 13:13:02 +0200

    Fixed expander

--- src/SprykerEco/Zed/Unzer/Business/Quote/UnzerQuoteExpander.php
+++ src/SprykerEco/Zed/Unzer/Business/Quote/UnzerQuoteExpander.php
@@ -8,6 +8,7 @@
 namespace SprykerEco\Zed\Unzer\Business\Quote;
 
 use Generated\Shared\Transfer\QuoteTransfer;
+use Generated\Shared\Transfer\UnzerPaymentResourceTransfer;
 use Generated\Shared\Transfer\UnzerPaymentTransfer;
 use SprykerEco\Shared\Unzer\UnzerConfig as SharedUnzerConfig;
 use SprykerEco\Zed\Unzer\Business\Reader\UnzerReaderInterface;
@@ -99,7 +100,7 @@ class UnzerQuoteExpander implements UnzerQuoteExpanderInterface
      */
     protected function addUnzerPayment(QuoteTransfer $quoteTransfer): QuoteTransfer
     {
-        $paymentResourceTransfer = $quoteTransfer->getPaymentOrFail()->getUnzerPaymentOrFail()->getPaymentResource();
+        $paymentResourceTransfer = $this->getUnzerPaymentResource($quoteTransfer);
         $paymentSelection = $quoteTransfer->getPaymentOrFail()->getPaymentSelectionOrFail();
 
         $unzerPaymentTransfer = (new UnzerPaymentTransfer())
@@ -110,5 +111,22 @@ class UnzerQuoteExpander implements UnzerQuoteExpanderInterface
         $quoteTransfer->getPaymentOrFail()->setUnzerPayment($unzerPaymentTransfer);
 
         return $quoteTransfer;
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
+     *
+     * @return \Generated\Shared\Transfer\UnzerPaymentResourceTransfer
+     */
+    protected function getUnzerPaymentResource(QuoteTransfer $quoteTransfer): UnzerPaymentResourceTransfer
+    {
+        if (
+            $quoteTransfer->getPaymentOrFail()->getUnzerPayment() !== null
+            && $quoteTransfer->getPaymentOrFail()->getUnzerPaymentOrFail()->getPaymentResource() !== null
+        ) {
+            return $quoteTransfer->getPaymentOrFail()->getUnzerPaymentOrFail()->getPaymentResourceOrFail();
+        }
+
+        return new UnzerPaymentResourceTransfer();
     }
 }
