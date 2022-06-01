6b9e0e6d664fd26bebe8371c2e439ad0d03e79c0
Kostiantyn Kichehlov <kostiantyn.kichehlov@spryker.com>
2022-06-01 10:57:53 +0200

    CC-14610 Added abstract entities

---
+++ src/SprykerEco/Zed/Unzer/Persistence/Propel/AbstractSpyPaymentUnzerCustomer.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * MIT License
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace SprykerEco\Zed\Unzer\Persistence\Propel;
+
+use Orm\Zed\Unzer\Persistence\Base\SpyPaymentUnzerCustomer as BaseSpyPaymentUnzerCustomer;
+
+/**
+ * Skeleton subclass for representing a row from the 'spy_payment_unzer_customer' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements. This class will only be generated as
+ * long as it does not already exist in the output directory.
+ */
+abstract class AbstractSpyPaymentUnzerCustomer extends BaseSpyPaymentUnzerCustomer
+{
+}
