95f2abdb2b73071d754b18dc050874b38f53a757
Michael Knappe <michael.knappe@spryker.com>
2022-07-13 18:28:43 +0200

    Adjusted payment method import to consider child merchants

--- src/SprykerEco/Zed/Unzer/Business/Import/UnzerPaymentMethodImporter.php
+++ src/SprykerEco/Zed/Unzer/Business/Import/UnzerPaymentMethodImporter.php
@@ -7,10 +7,16 @@
 
 namespace SprykerEco\Zed\Unzer\Business\Import;
 
+use Generated\Shared\Transfer\PaymentMethodsTransfer;
+use Generated\Shared\Transfer\PaymentMethodTransfer;
+use Generated\Shared\Transfer\UnzerCredentialsCollectionTransfer;
+use Generated\Shared\Transfer\UnzerCredentialsConditionsTransfer;
+use Generated\Shared\Transfer\UnzerCredentialsCriteriaTransfer;
 use Generated\Shared\Transfer\UnzerKeypairTransfer;
 use SprykerEco\Zed\Unzer\Business\ApiAdapter\UnzerPaymentMethodsAdapterInterface;
 use SprykerEco\Zed\Unzer\Business\Import\Adapter\PaymentImportAdapterInterface;
 use SprykerEco\Zed\Unzer\Business\Import\Filter\UnzerPaymentMethodImportFilterInterface;
+use SprykerEco\Zed\Unzer\Business\Reader\UnzerReaderInterface;
 use SprykerEco\Zed\Unzer\UnzerConfig;
 
 class UnzerPaymentMethodImporter implements UnzerPaymentMethodImporterInterface
@@ -18,39 +24,47 @@ class UnzerPaymentMethodImporter implements UnzerPaymentMethodImporterInterface
     /**
      * @var \SprykerEco\Zed\Unzer\UnzerConfig
      */
-    protected $unzerConfig;
+    protected UnzerConfig $unzerConfig;
 
     /**
      * @var \SprykerEco\Zed\Unzer\Business\Import\Filter\UnzerPaymentMethodImportFilterInterface
      */
-    protected $unzerPaymentMethodImportFilter;
+    protected UnzerPaymentMethodImportFilterInterface $unzerPaymentMethodImportFilter;
 
     /**
      * @var \SprykerEco\Zed\Unzer\Business\ApiAdapter\UnzerPaymentMethodsAdapterInterface
      */
-    protected $unzerPaymentMethodsAdapter;
+    protected UnzerPaymentMethodsAdapterInterface $unzerPaymentMethodsAdapter;
 
     /**
      * @var \SprykerEco\Zed\Unzer\Business\Import\Adapter\PaymentImportAdapterInterface
      */
-    protected $paymentImportAdapter;
+    protected PaymentImportAdapterInterface $paymentImportAdapter;
+
+    /**
+     * @var \SprykerEco\Zed\Unzer\Business\Reader\UnzerReaderInterface
+     */
+    protected UnzerReaderInterface $unzerReader;
 
     /**
      * @param \SprykerEco\Zed\Unzer\UnzerConfig $unzerConfig
      * @param \SprykerEco\Zed\Unzer\Business\Import\Filter\UnzerPaymentMethodImportFilterInterface $unzerPaymentMethodImportFilter
      * @param \SprykerEco\Zed\Unzer\Business\Import\Adapter\PaymentImportAdapterInterface $paymentImportAdapter
      * @param \SprykerEco\Zed\Unzer\Business\ApiAdapter\UnzerPaymentMethodsAdapterInterface $unzerPaymentMethodsAdapter
+     * @param \SprykerEco\Zed\Unzer\Business\Reader\UnzerReaderInterface $unzerReader
      */
     public function __construct(
         UnzerConfig $unzerConfig,
         UnzerPaymentMethodImportFilterInterface $unzerPaymentMethodImportFilter,
         PaymentImportAdapterInterface $paymentImportAdapter,
-        UnzerPaymentMethodsAdapterInterface $unzerPaymentMethodsAdapter
+        UnzerPaymentMethodsAdapterInterface $unzerPaymentMethodsAdapter,
+        UnzerReaderInterface $unzerReader
     ) {
         $this->unzerConfig = $unzerConfig;
         $this->unzerPaymentMethodImportFilter = $unzerPaymentMethodImportFilter;
         $this->paymentImportAdapter = $paymentImportAdapter;
         $this->unzerPaymentMethodsAdapter = $unzerPaymentMethodsAdapter;
+        $this->unzerReader = $unzerReader;
     }
 
     /**
@@ -61,6 +75,13 @@ class UnzerPaymentMethodImporter implements UnzerPaymentMethodImporterInterface
     public function performPaymentMethodsImport(UnzerKeypairTransfer $unzerKeypairTransfer): void
     {
         $paymentMethodsTransfer = $this->unzerPaymentMethodsAdapter->getPaymentMethods($unzerKeypairTransfer);
+        $childUnzerCredentialsCollectionTransfer = $this->getChildUnzerCredentialsCollectionTransfer($unzerKeypairTransfer);
+        foreach ($childUnzerCredentialsCollectionTransfer->getUnzerCredentials() as $childUnzerCredentialsTransfer) {
+            $paymentMethodsTransfer = $this->appendChildPaymentMethodsTransfer(
+                $paymentMethodsTransfer,
+                $this->unzerPaymentMethodsAdapter->getPaymentMethods($childUnzerCredentialsTransfer->getUnzerKeypair())
+            );
+        }
 
         if ($paymentMethodsTransfer->getMethods()->count() === 0) {
             return;
@@ -87,5 +108,52 @@ class UnzerPaymentMethodImporter implements UnzerPaymentMethodImporterInterface
         }
 
         $this->paymentImportAdapter->createPaymentMethods($paymentProviderTransfer, $paymentMethodTransfers);
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\PaymentMethodsTransfer $paymentMethodsTransfer
+     * @param \Generated\Shared\Transfer\PaymentMethodsTransfer $childPaymentMethodsTransfer
+     *
+     * @return \Generated\Shared\Transfer\PaymentMethodsTransfer
+     */
+    protected function appendChildPaymentMethodsTransfer(
+        PaymentMethodsTransfer $paymentMethodsTransfer,
+        PaymentMethodsTransfer $childPaymentMethodsTransfer
+    ): PaymentMethodsTransfer {
+        $unzerPaymentMethodKeys = $this->getPaymentMethodKeys($paymentMethodsTransfer);
+
+        foreach ($childPaymentMethodsTransfer->getMethods() as $paymentMethodTransfer) {
+            if (!in_array($paymentMethodTransfer->getPaymentMethodKey(), $unzerPaymentMethodKeys)) {
+                $paymentMethodsTransfer->getMethods()->append($paymentMethodTransfer);
+            }
+        }
+
+        return $paymentMethodsTransfer;
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\UnzerKeypairTransfer $unzerKeypairTransfer
+     *
+     * @return \Generated\Shared\Transfer\UnzerCredentialsCollectionTransfer
+     */
+    protected function getChildUnzerCredentialsCollectionTransfer(
+        UnzerKeypairTransfer $unzerKeypairTransfer
+    ): UnzerCredentialsCollectionTransfer {
+        $unzerCredentialsConditionsTransfer = (new UnzerCredentialsConditionsTransfer())->addParentId($unzerKeypairTransfer->getIdUnzerCredentials());
+        $unzerCredentialsCriteriaTransfer = (new UnzerCredentialsCriteriaTransfer())->setUnzerCredentialsConditions($unzerCredentialsConditionsTransfer);
+
+        return $this->unzerReader->getUnzerCredentialsCollectionByCriteria($unzerCredentialsCriteriaTransfer);
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\PaymentMethodsTransfer $paymentMethodsTransfer
+     *
+     * @return array<string|null>
+     */
+    protected function getPaymentMethodKeys(PaymentMethodsTransfer $paymentMethodsTransfer): array
+    {
+        return array_map(function (PaymentMethodTransfer $paymentMethodTransfer) {
+            return $paymentMethodTransfer->getPaymentMethodKey();
+        }, (array)$paymentMethodsTransfer->getMethods());
     }
 }
